7c23f16b8bd6c4de1b922611e0952ef0b2f59309
mubarak23 <mubarakaminu340@gmail.com>
2020-01-16 13:03:24 +0100

    complete connection with mongdb

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -1,2 +1,13 @@
 import mongoose from 'mongoose';
 import config from './index';
+
+const URL = config.mongoURI;
+mongoose.connect(URL);
+
+mongoose.connection.on('connected', () => {
+  console.log('Established MongoDB Default connection');
+});
+
+mongoose.connection.on('err', err => {
+  console.log('Established MongoDB Default connection Error:' + err);
+});
