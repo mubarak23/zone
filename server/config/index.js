cc58a261916d6cbc85338c4e18071dd1c0cfd71a
mubarak23 <mubarakaminu340@gmail.com>
2020-01-16 12:58:49 +0100

    dbconnnection setup

--- server/config/index.js
+++ server/config/index.js
@@ -1,5 +1,7 @@
 const config = {
-    port: process.env.PORT || 4000,
-    jwtScrete: process.env.JWT_SCRETE || 'mkT23j#u!45',
-    
-}
+  port: process.env.PORT || 4000,
+  jwtScrete: process.env.JWT_SCRETE || 'mkT23j#u!45',
+  mongoURI: process.env.MONGOURI
+};
+
+export default config;
