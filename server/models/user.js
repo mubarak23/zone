7c70ecc3fe3d98cdab5ac66abc9654d1851e97ad
mubarak23 <mubarakaminu340@gmail.com>
2020-01-16 20:48:19 +0100

    complete model

--- server/models/user.js
+++ server/models/user.js
@@ -12,6 +12,57 @@ const userSchema = new Schema({
   email: {
     type: String,
     trim: true,
-    unique: 'Email already Exists'
+    unique: 'Email already Exists',
+    match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+    required: 'Email Address is required'
+  },
+  hashPassword: {
+    type: String,
+    required: 'Paasword is Required'
+  },
+  salt: {
+    type: String
   }
 });
+
+userSchema
+  .virtual('password')
+  .set(function(password) {
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashPassword = this.encryptedPassword(password);
+  })
+  .get(function() {
+    return this._password;
+  });
+
+userSchema.method = {
+  authenticate: function(plainText) {
+    return this.encryptedPassword(plainText) === this.hashPassword;
+  },
+  encryptedPassword: function(password) {
+    if (!password) return '';
+    try {
+      return crypto
+        .createHmac('sha1', this.salt)
+        .update(password)
+        .digest('hex');
+    } catch (err) {
+      return '';
+    }
+  },
+  makeSalt: function() {
+    return Math.round(new Date().valueOf() * Math.random()) + '';
+  }
+};
+
+userSchema.path('hashedPassword').validate(function(v) {
+  if (this.hashPassword && this._password.length < 6) {
+    this.invalidate('password', 'Password must be at least 6 character long.');
+  }
+  if (this.isNew && !this._password) {
+    this.invalidate('password', 'Password is required.');
+  }
+}, null);
+
+export default mongoose.model('Users', userSchema);
