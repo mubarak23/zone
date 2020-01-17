177b4f7fefe527997150e56073ec06808d8a31f0
mubarak23 <mubarakaminu340@gmail.com>
2020-01-17 13:54:54 +0100

    start working on the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,43 +2,10 @@ import React from 'react';
 import './App.css';
 import Todos from './components/Todos';
 
-class App extends React.Component{
-  state = {
-    todos: [
-      {
-        id: 1,
-        title: 'create project component in pms ui',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Correct exercise tracker api and ui connect error',
-        completed: false
-      },
-      {
-        id: 3,
-        title: 'Telecommunication and Advance Circuit Technique studies',
-        completed: true
-      }
-    ]
-  }
-  markComplete = (id) =>{
-    this.setState({
-      todos: this.state.todos.map((todo) =>{
-        if(todo.id === id){
-          todo.completed = !todo.completed
-        }
-        return todo;
-      })
-    });
-  }
-  render(){
+class App extends React.Component {
+  render() {
     //console.log(this.state.todos);
-    return(
-      <div>
-        <Todos todos={this.state.todos}  markComplete={this.markComplete}/>
-      </div>
-    )
+    return <div>Welcome to Zone App</div>;
   }
-} 
+}
 export default App;
