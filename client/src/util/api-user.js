0deb3027a1ace82acb0f1c9e101d30da2d9b0f0f
mubarak23 <mubarakaminu340@gmail.com>
2020-01-18 10:59:26 +0100

    add user api calls

--- client/src/util/api-user.js
+++ client/src/util/api-user.js
@@ -1,3 +1,53 @@
-export const register = user =>{
-    
+import { response } from 'express';
+
+export const register = user => {
+  return fetch('api/user', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify(user)
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => {
+      console.log('error has occur');
+      return err;
+    });
+};
+
+export const findUserByProfile = (params, credentials) => {
+  return fetch('/api/user/' + params.userId, {
+    method: 'get',
+    headers: {
+      Accept: 'application/json',
+      'Content-type': 'application/json',
+      Authorization: 'Bearer ' + credentials.t
+    }
+  })
+    .then(response => {
+      return response.json();
+    })
+    .catch(err => {
+      console.log(err);
+      return err;
+    });
+};
+
+export const deleteUser = (params, credentials) =>{
+    return fetch('/api/user/' + params.userId, {
+        method: 'DELETE',
+        headers: {
+            Accept: 'application/json',
+                'Content-type': 'application/json',
+                Authorization: 'Bearer ' + credentials.t
+        }
+    }).then(response => {
+        return response.json();
+    }).catch(err =>{
+        console.log(err);
+        return err
+    })
 }
