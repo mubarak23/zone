14da16185ab53c6ae1cee99cd0f6366ae9ed9220
mubarak23 <mubarakaminu340@gmail.com>
2020-01-18 11:21:21 +0100

    code auth check helper

---
+++ client/src/components/auth/auth-help.js
@@ -0,0 +1,32 @@
+import { signout } from '../../util/api-auth';
+
+const auth = {
+  iaAuthenticated() {
+    if (typeof window === 'undefined') return false;
+
+    if (sessionStorage.getItem('jwt')) {
+      return JSON.parse(sessionStorage.getItem('jwt'));
+    } else {
+      return false;
+    }
+  },
+  authenticate(jwt, cb) {
+    if (typeof window !== 'undefined') {
+      sessionStorage.setItem('jwt', JSON.stringify(jwt));
+    }
+    cb();
+  },
+  signout(cb) {
+    if (typeof window !== 'undefined') {
+      sessionStorage.removeItem('jwt');
+      cb();
+
+      //if possible
+      signout().then(data => {
+        document.cookie = 't=, expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+      });
+    }
+  }
+};
+
+export default auth;
