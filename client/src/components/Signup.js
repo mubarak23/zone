9a1e44a5238ad6eba764900fe28abbf2a6bcdc7c
mubarak23 <mubarakaminu340@gmail.com>
2020-01-19 03:20:36 +0100

    completed signup component

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -16,27 +16,139 @@ import { Link } from 'react-router-dom';
 import { registerUser } from '../util/api-user';
 
 const styles = theme => ({
-	card: {
-		maxWidth: 600,
-		margin: 'auto',
-		textAlign: 'center',
-		marginTop: theme.spacing.unit * 5,
-		paddingBottom: theme.spacing.unit * 2
-	},
-	error: {
-		verticalAlign: 'middle'
-	},
-	title: {
-		marginTop: theme.spacing.unit * 2,
-		color: theme.palette.openTitle
-	},
-	textField: {
-		marginLeft: theme.spacing.unit,
-		marginRight: theme.spacing.unit,
-		width: 300
-	},
-	submit: {
-		margin: 'auto',
-		marginBottom: theme.spacing.unit * 2
-	}
+  card: {
+    maxWidth: 600,
+    margin: 'auto',
+    textAlign: 'center',
+    marginTop: theme.spacing.unit * 5,
+    paddingBottom: theme.spacing.unit * 2
+  },
+  error: {
+    verticalAlign: 'middle'
+  },
+  title: {
+    marginTop: theme.spacing.unit * 2,
+    color: theme.palette.openTitle
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 300
+  },
+  submit: {
+    margin: 'auto',
+    marginBottom: theme.spacing.unit * 2
+  }
 });
+
+class Signup extends Component {
+  state = {
+    name: '',
+    password: '',
+    email: '',
+    open: '',
+    error: ''
+  };
+
+  handleChange = name => event => {
+    this.setState({ ['name']: event.target.value });
+  };
+
+  clicksubmit = () => {
+    const user = {
+      name: this.state.name || undefined,
+      email: this.state.email || undefined,
+      password: this.state.password || undefined
+    };
+
+    registerUser(user).then(data => {
+      if (data.error) {
+        this.setState({ error: data.error });
+      } else {
+        this.setState({ open: true, error: '' });
+      }
+    });
+  };
+  render() {
+    const { classes } = this.props;
+    return (
+      <div>
+        <Card className={classes.card}>
+          <CardContent>
+            <Typography
+              type='headline'
+              component='h2'
+              className={classes.title}
+            >
+              Sign Up
+            </Typography>
+            <TextField
+              id='name'
+              label='Name'
+              className={classes.textField}
+              value={this.state.name}
+              onChange={this.handleChange('name')}
+              margin='normal'
+            />
+            <br />
+            <TextField
+              id='email'
+              type='email'
+              label='Email'
+              className={classes.textField}
+              value={this.state.email}
+              onChange={this.handleChange('email')}
+              margin='normal'
+            />
+            <br />
+            <TextField
+              id='password'
+              type='password'
+              label='Password'
+              className={classes.textField}
+              value={this.state.password}
+              onChange={this.handleChange('password')}
+              margin='normal'
+            />
+            <br />{' '}
+            {this.state.error && (
+              <Typography component='p' color='error'>
+                <Icon color='error' className={classes.error}>
+                  error
+                </Icon>
+                {this.state.error}
+              </Typography>
+            )}
+          </CardContent>
+          <CardActions>
+            <Button
+              color='primary'
+              variant='raised'
+              onClick={this.clickSubmit}
+              className={classes.submit}
+            >
+              Sign Up
+            </Button>
+          </CardActions>
+        </Card>
+        <Dialog open={this.state.open} disableBackdropClick={true}>
+          <DialogTitle>New Account</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              New account successfully created.
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Link to='/signin'>
+              <Button color='primary' autoFocus='autoFocus' variant='raised'>
+                Sign In
+              </Button>
+            </Link>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(Signup);
