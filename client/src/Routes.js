993b3937bd942aa5ba06932ac8706db9bd468622
mubarak23 <mubarakaminu340@gmail.com>
2020-01-20 08:20:29 +0100

    add navbar and private route

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
+import PrivateRoutes from './components/PrivateRoute';
 
 class Routes extends Component {
   render() {
