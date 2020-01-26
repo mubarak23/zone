cc3490b94672da0f5213d13cfb705fc336564501
mubarak23 <mubarakaminu340@gmail.com>
2020-01-26 13:47:00 +0100

    correct model

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -23,31 +23,35 @@ exports.registerUser = (req, res, next) => {
   });
 };
 
-exports.createUser = (req, res) => {
+exports.createUser = async (req, res) => {
   //return res.json(req.body);
-  const { name, email, password } = req.body;
-  //return res.json(name);
-  const user = new Users({
-    name,
-    email,
-    password
-  });
-
-  const salt = bcrypt.genSalt(10);
-  return res.json();
-  user.password = bcrypt.hash(password, salt);
-  user
-    .save()
-    .then(user => {
-      return res.status(200).json({
-        user
-      });
-    })
-    .catch(error => {
-      return res.status(400).json({
-        error
-      });
+  try {
+    const { name, email, password } = req.body;
+    //return res.json(name);
+    const user = new Users({
+      name,
+      email,
+      password
     });
+
+    const salt = await bcrypt.genSalt(10);
+    //return res.json(salt);
+    user.password = await bcrypt.hash(password, salt);
+    user
+      .save()
+      .then(user => {
+        return res.status(200).json({
+          user
+        });
+      })
+      .catch(error => {
+        return res.status(400).json({
+          data: 'Error has occurs'
+        });
+      });
+  } catch (err) {
+    return res.send(err);
+  }
 };
 
 exports.findUserById = (req, res, next, id) => {
--- server/models/user.js
+++ server/models/user.js
@@ -3,60 +3,7 @@ const crypto = require('crypto');
 
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  name: {
-    type: String,
-    trim: true,
-    required: 'User Name is required'
-  },
-  email: {
-    type: String,
-    trim: true,
-    unique: 'Email already Exists',
-    match: [/.+\@.+\..+/, 'Please fill a valid email address'],
-    required: 'Email Address is required'
-  },
-  hashPassword: {
-    type: String,
-    required: 'Paasword is Required'
-  },
-  salt: {
-    type: String
-  }
-});
-
-userSchema
-  .virtual('password')
-  .set(function(password) {
-    this._password = password;
-    this.salt = this.makeSalt();
-    this.hashPassword = this.encryptedPassword(password);
-  })
-  .get(function() {
-    return this._password;
-  });
-
-userSchema.method = {
-  authenticate: function(plainText) {
-    return this.encryptedPassword(plainText) === this.hashPassword;
-  },
-  encryptedPassword: function(password) {
-    if (!password) return '';
-    try {
-      return crypto
-        .createHmac('sha1', this.salt)
-        .update(password)
-        .digest('hex');
-    } catch (err) {
-      return '';
-    }
-  },
-  makeSalt: function() {
-    return Math.round(new Date().valueOf() * Math.random()) + '';
-  }
-};
-
-const mainUserSchema = new Schema({
+const UserSchema = new Schema({
   name: {
     type: String,
     required: true
@@ -72,4 +19,4 @@ const mainUserSchema = new Schema({
   }
 });
 
-module.exports = mongoose.model('Users', mainUserSchema);
+module.exports = mongoose.model('Users', UserSchema);
